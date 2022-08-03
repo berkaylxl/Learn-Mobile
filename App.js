ad4c017fbc3a2e5b5c49890a22af0c553aa95046
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-08-04 00:53:17 +0300

    basic func

--- App.js
+++ App.js
@@ -5,26 +5,51 @@ import React from 'react';
 import {
   StyleSheet,
   View,
-  Image,
-  B
+  Text,
+  Button,
+  Alert
 } from 'react-native';
 
-const App =()=>{
+const App = () => {
+  
+  const CreateKey = () => {
+    var key = ""
+    var characters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuopasdfghjklizxcvbnm0123456789!?'
+    var keys = []
+    var charactersLength = characters.length
 
-  return(
-      <View style={{flex:1,flexDirection:'column'}}>
-      
-    </View>
+    for (let index = 0; index < 25; index++) {
 
-  )
-   
-  }
-  const styles =StyleSheet.create({
-    container:{
-      backgroundColor:'yellow',
-      flex:1
+      keys.push(characters.charAt(Math.floor((Math.random() * charactersLength))))
     }
-  })
-
+    key=keys.join('')
+    Alert.alert
+    (
+      "Key Anahtarınız:",
+      key
+    )
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.key}>Random Key </Text>
+      <Button
+        onPress={CreateKey}
+        title="Get Key"
+        color="#841584"
+      />
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: 'grey',
+    flex: 1
+  },
+  key: {
+    fontSize: 20,
+    marginTop: 10
+  },
+})
 
 export default App;
