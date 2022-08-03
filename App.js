66a8b2ae70fd006d96d011c18e749a65a9c28d11
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-08-03 15:52:23 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Image,
+  B
+} from 'react-native';
+
+const App =()=>{
+
+  return(
+      <View style={{flex:1,flexDirection:'column'}}>
+      
+    </View>
+
+  )
+   
+  }
+  const styles =StyleSheet.create({
+    container:{
+      backgroundColor:'yellow',
+      flex:1
+    }
+  })
+
+
+export default App;
