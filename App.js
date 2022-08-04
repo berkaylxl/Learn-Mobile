fad7ed5beedd415380103ebce56f8ee18e63730b
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-08-04 22:16:49 +0300

    login screen deneme

--- App.js
+++ App.js
@@ -1,55 +1,61 @@
 
-
-import React from 'react';
-
+import React,{useState} from 'react';
+import { styles } from './Style'
 import {
-  StyleSheet,
+  SafeAreaView,
   View,
   Text,
   Button,
-  Alert
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 const App = () => {
-  
+   const [key, setkey] = useState("")
   const CreateKey = () => {
-    var key = ""
-    var characters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuopasdfghjklizxcvbnm0123456789!?'
     var keys = []
+    var characters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuopasdfghjklizxcvbnm0123456789!?'
     var charactersLength = characters.length
 
     for (let index = 0; index < 25; index++) {
-
       keys.push(characters.charAt(Math.floor((Math.random() * charactersLength))))
     }
-    key=keys.join('')
-    Alert.alert
-    (
-      "Key Anahtarınız:",
-      key
-    )
+    setkey(keys.join(''))
   }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={{flex:1,backgroundColor:"white",borderBottomWidth:2,}}>
       <Text style={styles.key}>Random Key </Text>
       <Button
+        style={{margin:10}}
         onPress={CreateKey}
         title="Get Key"
-        color="#841584"
-      />
-    </View>
+        color="#841584"></Button>
+        <Text style={{
+          margin:10,
+          borderWidth:1,
+          padding:10,
+          borderColor:"purple",fontWeight:'bold'}}>{key}</Text>
+      </View>
+       {/* -------------------------------------Login View*/}
+      <View style={{flex:2,alignItems:'center',justifyContent:"center"}}>
+       <TextInput 
+       placeholder='UserName' 
+       style={styles.logintexttBox}>
+
+       </TextInput>
+       <TextInput
+        secureTextEntry={true}
+        placeholder='Password' 
+        style={styles.logintexttBox}>
+        </TextInput>
+        <TouchableOpacity style={styles.touchable}>
+          <Text style={{textAlign:'center',color:"white"}}>Login</Text>
+        </TouchableOpacity>
+
+      </View>
+    </SafeAreaView>
   )
 }
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: 'grey',
-    flex: 1
-  },
-  key: {
-    fontSize: 20,
-    marginTop: 10
-  },
-})
-
 export default App;
