ed1f17c9eb1f790f9cb5e1b3ed30140ae4a7735f
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-08-04 22:15:33 +0300

    login screen deneme

---
+++ Style.js
@@ -0,0 +1,36 @@
+import {
+    StyleSheet,
+  } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flexDirection:"column",
+      backgroundColor: 'white',
+      flex: 1,
+      
+    },
+    key: {
+      textAlign:'center',
+      fontSize: 20,
+      marginTop: 10,
+      marginBottom:10
+    },
+    logintexttBox:{
+      borderWidth:1,
+      borderColor:'blue',
+      borderRadius:30,
+      width:'80%',
+      textAlign:'center',
+      margin:10
+    },
+    touchable:{
+      borderWidth:1,
+      borderColor:'blue',
+      borderRadius:30,
+      width:'80%',
+      padding:10,
+      
+      backgroundColor:'blue'
+    }
+   
+  })
